25eb6411163dc5943a674b024e6698c9f45fed8a
Christian Oliveros <christianol_01@hotmail.com>
2019-10-02 13:52:49 -0400

    Added RValue Add and Modify to Priority Queue

--- global/priority_queue.hpp
+++ global/priority_queue.hpp
@@ -9,6 +9,7 @@
 #include <cstdio>
 #include <cassert>
 #include <cstdlib>
+#include <utility>
 
 template <typename T>
 class PriorityQueue
@@ -16,6 +17,7 @@ class PriorityQueue
 public:
     PriorityQueue();
     int Add(int f, int g, const T& data);
+    int Add(int f, int g, const T&& data);
     const T& Top() const;
     int CurrentPriority() const;
     bool Empty() const;
@@ -23,6 +25,7 @@ public:
     void Clear();
 
     void Modify(int f, int g, int index, const T& data);
+    void Modify(int f, int g, int index, const T&& data);
 
 private:
     /* Min priority queue on f. 
@@ -33,9 +36,11 @@ private:
         void Pop();
         bool Empty() const;
         int Add(int g, const T& data);
+        int Add(int g, const T&& data);
         const T& Top() const;
         void Clear();
         void Modify(int g, int index, const T& data);
+        void Modify(int g, int index, const T&& data);
     private:
         std::vector<std::vector<T> > m_stacks;
     };
@@ -75,6 +80,15 @@ int PriorityQueue<T>::Bucket::Add(int g, const T& data)
 }
 
 template<typename T>
+int PriorityQueue<T>::Bucket::Add(int g, const T&& data)
+{
+    if (m_stacks.size() <= (size_t)g)
+        m_stacks.resize(g + 1);
+    m_stacks[g].push_back(std::move(data));
+    return m_stacks[g].size() - 1;
+}
+
+template<typename T>
 void PriorityQueue<T>::Bucket::Modify(int g, int index, const T& data)
 {
     // if (g >= (int)m_stacks.size() || index >= m_stacks[g].size()) {
@@ -83,6 +97,17 @@ void PriorityQueue<T>::Bucket::Modify(int g, int index, const T& data)
     assert(g < (int)m_stacks.size());
     assert(index < (int)m_stacks[g].size());
     m_stacks[g][index] = data;
+}
+
+template<typename T>
+void PriorityQueue<T>::Bucket::Modify(int g, int index, const T&& data)
+{
+    // if (g >= (int)m_stacks.size() || index >= m_stacks[g].size()) {
+    //     printf("%d %d | %d %d\n", g, index, m_stacks.size(), m_stacks[g].size());
+    // }
+    assert(g < (int)m_stacks.size());
+    assert(index < (int)m_stacks[g].size());
+    m_stacks[g][index] = std::move(data);
 }
 
 template<typename T>
@@ -107,6 +132,13 @@ void PriorityQueue<T>::Modify(int f, int g, int index, const T& data)
 }
 
 template<typename T>
+void PriorityQueue<T>::Modify(int f, int g, int index, const T&& data)
+{
+    assert(f < (int)m_buckets.size());
+    m_buckets[f].Modify(g, index, std::move(data));
+}
+
+template<typename T>
 int PriorityQueue<T>::Add(int f, int g, const T& data)
 {
     assert(f >= 0);
@@ -123,6 +155,25 @@ int PriorityQueue<T>::Add(int f, int g, const T& data)
     if (f < m_at)
         m_at = f;
     return m_buckets[f].Add(g, data);
+}
+
+template<typename T>
+int PriorityQueue<T>::Add(int f, int g, const T&& data)
+{
+    assert(f >= 0);
+    if (f >= m_max) {
+        // double size until we can accomodate f
+        int new_max = m_max;
+        while (f >= new_max)
+            new_max *= 2;
+        if (m_at == m_max)
+            m_at = new_max;
+        m_max = new_max;
+        m_buckets.resize(new_max);
+    }
+    if (f < m_at)
+        m_at = f;
+    return m_buckets[f].Add(g, std::move(data));
 }
 
 template<typename T>
--- global/ucs.cpp
+++ global/ucs.cpp
@@ -58,7 +58,6 @@ int main(int argc, char **argv) {
   std::shared_ptr<Node> node = Node::CreateNode(state, nullptr, -1);
   open.Add(0, 0, node);
   node = nullptr;
-  std::shared_ptr<Node> child_node;
 
   int parent_cost = -1;
   int child_new_cost = -1;
@@ -97,19 +96,15 @@ int main(int argc, char **argv) {
         state_map_add(color_map, &child, static_cast<int>(Color::Gray));
         // Update Cost
         state_map_add(cost_map, &child, child_new_cost);
-        // Add with proprity
-        child_node = node->MakeNode(child, ruleid);
-        open.Add(child_new_cost, child_new_cost, child_node);
-        child_node = nullptr;
+        // Add with priority
+        open.Add(child_new_cost, child_new_cost, node->MakeNode(child, ruleid));
       } else if (child_new_cost < *child_cost) {
         // update open child
         assert(static_cast<Color>(*child_color) == Color::Gray);
         // Update Cost
         state_map_add(cost_map, &child, child_new_cost);
-        // Add with proprity
-        child_node = node->MakeNode(child, ruleid);
-        open.Add(child_new_cost, child_new_cost, child_node);
-        child_node = nullptr;
+        // Add with priority
+        open.Add(child_new_cost, child_new_cost, node->MakeNode(child, ruleid));
       }
     }
 
