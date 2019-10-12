7e22a9e05dc03b0186d67d3393676ffeed69cb8b
Christian Oliveros <christianol_01@hotmail.com>
2019-10-12 11:25:54 -0400

    IDA* PDB

---
+++ global/ida_pdb.cpp
@@ -0,0 +1,104 @@
+#include <assert.h>
+#include <cstdint>
+#include <inttypes.h>
+#include <limits>
+#include <queue>
+#include <stdio.h>
+#include <stdlib.h>
+#include <utility>
+
+
+#include "node.hpp"
+#include "pdb_handler.hpp"
+#include "read_state.hpp"
+
+
+PDBHandler pdbs;
+
+long long unsigned heuristic(const state_t *from) {
+  return pdbs.GetValue(from);
+}
+
+/* Avoid sending constants through stack */
+long long unsigned bound = 0;
+/* Avoid save useless stuff in stack (we don't save them because we can
+ * regenerate them) */
+int ruleid;       // Generated Rule id
+state_t child;    // Generated Child
+int childHistory; // Generated Child History
+std::pair<std::shared_ptr<Node>, long long unsigned>
+    calc; // Calculation made by ida
+
+std::pair<std::shared_ptr<Node>, long long unsigned>
+ida(std::shared_ptr<Node> node) {
+  // Nodes just after the bound
+  if (node->GetCost() > bound) {
+    return std::make_pair<std::shared_ptr<Node>, long long unsigned>(
+        std::shared_ptr<Node>(nullptr), node->GetCost());
+  }
+
+  if (is_goal(node->GetState())) {
+    long long unsigned cost = node->GetCost();
+    return std::make_pair(std::move(node), cost);
+  }
+
+  long long unsigned minFailedCost =
+      std::numeric_limits<long long unsigned>::max();
+  ruleid_iterator_t iter; // ruleid_terator_t is the type defined by the PSVN
+
+  // LOOP THOUGH THE CHILDREN ONE BY ONE
+  init_fwd_iter(&iter, node->GetState()); // initialize the child iterator
+  while ((ruleid = next_ruleid(&iter)) >= 0) {
+
+    // Check for history pruning
+    if (fwd_rule_valid_for_history(node->GetHistory(), ruleid) == 0) {
+      continue;
+    }
+
+    // Generate Child
+    apply_fwd_rule(ruleid, node->GetState(), &child);
+    childHistory = next_fwd_history(node->GetHistory(), ruleid);
+
+    calc = ida(node->MakeNode(child, ruleid, childHistory,
+                              heuristic(node->GetState())));
+
+    // found it
+    if (calc.first != nullptr) {
+      return calc;
+    }
+
+    // failed to find it
+    if (calc.second < minFailedCost) {
+      minFailedCost = calc.second;
+    }
+  }
+  return std::make_pair(std::shared_ptr<Node>(nullptr), minFailedCost);
+}
+
+int main(int argc, const char **argv) {
+
+  if (!pdbs.Load(argc, argv, 1)) {
+    return -1;
+  }
+
+  state_t state;
+  if (!readState(&state)) {
+    return -1;
+  }
+
+  int currHistory = init_history;
+  std::shared_ptr<Node> node =
+      Node::CreateNode(state, nullptr, -1, currHistory);
+
+  while (true) {
+    calc = ida(node);
+
+    if (calc.first != nullptr) {
+      break;
+    }
+    bound += calc.second;
+  }
+
+  // Print path
+  PrintPath(calc.first);
+}
---
+++ global/pdb_handler.hpp
@@ -0,0 +1,136 @@
+#pragma once
+#ifndef PDB_HANDLER_HPP
+#define PDB_HANDLER_HPP 1
+
+#include <cstdio>
+#include <cstring>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+#include <cassert>
+
+
+class PDBHandler {
+public:
+  PDBHandler() = default;
+  PDBHandler(PDBHandler &&) = default;
+  PDBHandler(const PDBHandler &) = delete;
+  PDBHandler &operator=(PDBHandler &&) = default;
+  PDBHandler &operator=(const PDBHandler &) = delete;
+  ~PDBHandler() = default;
+
+  /* Load PBBs from the arguments passed to the program. Startpos is the
+   * position from which start reading the PDBs names. The first argument must
+   * always be 'm' (max) or 'a' (addition) to select the heuristic function */
+  bool Load(int argc, const char **argv, int startPos = 1) {
+
+    if (startPos >= argc - 1) {
+      std::printf("Not enough arguments\n");
+      std::printf("Arguments must be: m|a [pdb_name]+");
+      return false;
+    }
+
+    if (strlen(argv[startPos]) != 1) {
+      std::printf("The first argument must always be 'm' (max) or 'a' "
+                  "(addition) to select the heuristic function");
+      std::printf(" but it was %s\n", argv[startPos]);
+      return false;
+    }
+
+    m_useMax = std::tolower(argv[startPos][0]) == 'm';
+
+    if (!m_useMax) {
+      if (std::tolower(argv[startPos][0]) != 'a') {
+        std::printf("The first argument must always be 'm' (max) or 'a' "
+                    "(addition) to select the heuristic function");
+        std::printf(" but it was %s\n", argv[startPos]);
+      }
+      std::printf("Using an Addition Heuristic\n");
+    } else {
+      std::printf("Using an Maximum Heuristic\n");
+    }
+
+    m_pdbs.reserve(argc - startPos - 1);
+
+    for (int i = startPos + 1; i < argc; ++i) {
+      PDB pdb;
+
+      if (!pdb.Load(argv[i])) {
+        return false;
+      }
+
+      m_pdbs.push_back(std::move(pdb));
+    }
+    return true;
+  }
+
+  /* Get the Heuristic Value using PDBs for a state */
+  long long unsigned GetValue(const state_t *state) {
+    long long unsigned res = 0;
+    for(size_t i=0; i<m_pdbs.size(); ++i) {
+      res += m_pdbs[i].GetValue(&m_asbtractStateHolder, state);
+    }
+    return res;
+  }
+
+private:
+  using PDB = struct PDB {
+    /* Abstraction used by the pdb */
+    std::unique_ptr<abstraction_t> m_abstraction;
+    /* PDB heuristic */
+    std::unique_ptr<state_map_t> m_map;
+
+    /* Max File Name Size*/
+    constexpr static unsigned k_maxFileNameSize = 1024;
+
+    PDB() = default;
+    PDB(PDB &&) = default;
+    PDB(const PDB &) = delete;
+    PDB &operator=(PDB &&) = default;
+    PDB &operator=(const PDB &) = delete;
+    ~PDB() = default;
+
+    /* Load the PDB by using its name (without extension) */
+    bool Load(const char *pdb_name) {
+      char pdb_fname[k_maxFileNameSize], abst_fname[k_maxFileNameSize];
+
+      strcpy(pdb_fname, pdb_name);
+      strcat(pdb_fname, ".pdb");
+      strcpy(abst_fname, pdb_name);
+      strcat(abst_fname, ".abst");
+
+      printf("Reading pdb into memory: abst=%s\n", abst_fname);
+      m_abstraction.reset(read_abstraction_from_file(abst_fname));
+
+      printf("Reading pdb into memory: pdb=%s\n", pdb_fname);
+      FILE *pdb_file = fopen(pdb_fname, "r");
+      assert(pdb_file != nullptr);
+      m_map.reset(read_state_map(pdb_file));
+      fclose(pdb_file);
+      return true;
+    }
+
+    /* Get PDB Value from state. (asbtractStateHolder is used to avoid to
+     * allocate many temp states, considered garbage for reuse after this call)
+     */
+    int GetValue(state_t *asbtractStateHolder, const state_t *state) {
+      // compute PDB value for state
+      abstract_state(m_abstraction.get(), state, asbtractStateHolder);
+      const int *pdb_value = state_map_get(m_map.get(), asbtractStateHolder);
+      assert(pdb_value != nullptr);
+      return *pdb_value;
+    }
+  };
+
+  /* State to avoid allocating temp states */
+  state_t m_asbtractStateHolder;
+
+  /* If we are using max (true) or addition (false) */
+  bool m_useMax = true;
+
+  /* PDBs loaded */
+  std::vector<PDB> m_pdbs;
+};
+
+#endif // !PDB_HANDLER_HPP
